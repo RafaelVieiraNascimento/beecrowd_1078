f059ae0fb7c17d682b10d415fd70c116e0d36d1e
RafaelVN <contato@RafaelVN.com>
2022-08-10 13:23:58 -0300

    beecrowd_1078

---
+++ src/program/Main.java
@@ -0,0 +1,30 @@
+package program;
+
+/*
+beecrowd | 1078 - Tabuada
+
+Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+1 x N = N      2 x N = 2N        ...       10 x N = 10N
+
+Entrada
+A entrada contém um valor inteiro N (2 < N < 1000).
+
+Saída
+Imprima a tabuada de N, conforme o exemplo fornecido.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i + " x " + n + " = " + i * n);
+		}
+		sc.close();
+	}
+}
